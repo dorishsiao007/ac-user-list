1e09aa945ebab3ad4ad9ae3514f5d4d75ee920f4
Doris Hsiao <dorishsiao007@gmail.com>
2020-03-19 11:34:06 +0800

    add pagination

--- src/index.js
+++ src/index.js
@@ -2,26 +2,63 @@
   // variable
   const baseUrl = 'https://lighthouse-user-api.herokuapp.com/api/v1/users/'
   const data = []
+
   const userList = document.querySelector('.user-list')
+  const pagination = document.querySelector('#pagination')
+
+  const itemPrePage = 12
   
+  //----------- listener event -------------
   // listener More Detail button
   userList.addEventListener('click', (event) => {
     if (event.target.matches('.more-detail')) {
       showUserDetail(event.target.dataset.id)
     }
   })
+
+  // listener pagination event
+  pagination.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      let page = event.target.dataset.page
+      getPaginationData(page, data)
+    }
+  })
   
+  //------------ main page: axios get api data -----------------
   // get api all user info
   axios.get(baseUrl)
     .then((res) => {
       data.push(...res.data.results)
-      console.log(data)
-      generateUserListHtml(data)
+      //generateUserCardHtml(data)
+      totalPagesHtml(data)
+      getPaginationData(1, data)
   })
     .catch((err) => {
       console.log(err)
   })
   
+  // ------------ function ---------------
+  // function: get pagination data
+  function getPaginationData(page, data){
+    let offSet = (page - 1) * itemPrePage
+    let paginationDataList = data.slice(offSet, offSet + itemPrePage)
+    generateUserCardHtml(paginationDataList)
+  }
+
+  // function: create total pages html
+  function totalPagesHtml(data){
+    const totalPages = Math.ceil(data.length / itemPrePage) || 1
+    let pageHtmlContent = ''
+
+    for (let i = 0; i < totalPages; i++) {
+      pageHtmlContent += `
+        <li class="page-item"><a class="page-link" href="javascript:;" data-page="${i + 1}">${i + 1}</a></li>
+      `
+    }
+    pagination.innerHTML = pageHtmlContent
+  }
+
+
   // function: show user detail info
   function showUserDetail(id){
     // element variable
@@ -53,7 +90,7 @@
   }
   
   // function: generate user list html
-  function generateUserListHtml(data){
+  function generateUserCardHtml(data){
     let htmlContent = ''
     
     data.forEach((user) => {
@@ -61,9 +98,9 @@
       let name = user.name
       let id = user.id
       htmlContent += `
-        <div class="col-sm-2">
+        <div class="col-sm-3">
           <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-            <img class="card-img-top" src="${avatar}" alt="Card image cap">
+            <!--img class="card-img-top" src="${avatar}" alt="Card image cap"-->
             <div class="card-body text-center">
               <h5 class="card-title">${name}</h5>
               <!-- Button trigger modal -->
